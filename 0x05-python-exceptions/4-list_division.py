7fc55de376c3ba70288f908c1a54d0ce448aec3c
omarprocessor <wadaniinarais1@gmail.com>
2024-09-30 15:29:39 -0700

    task 4d oen

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            # Attempt to get the elements from both lists
+            num1 = my_list_1[i]
+            num2 = my_list_2[i]
+            try:
+                # Perform the division
+                division_result = num1 / num2
+            except ZeroDivisionError:
+                print("division by 0")
+                division_result = 0
+            except TypeError:
+                print("wrong type")
+                division_result = 0
+        except IndexError:
+            print("out of range")
+            division_result = 0
+        result.append(division_result)
+    return result
