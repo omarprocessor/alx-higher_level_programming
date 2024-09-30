f753a074f06d78a00475b98563c1493a68fdacc6
omarprocessor <wadaniinarais1@gmail.com>
2024-09-30 14:31:28 -0700

    task 0

--- 0x05-python-exception/0-safe_print_list.py
+++ 0x05-python-exception/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             hesabu += 1
-    except IndexError:
+    except (IndexError, TypeError):
         pass
     print()
     return hesabu
