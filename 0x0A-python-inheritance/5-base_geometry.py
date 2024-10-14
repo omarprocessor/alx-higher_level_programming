cb18e3e5ed8e3588aa4198aa85fd367f0a8844f9
omarprocessor <wadaniinarais1@gmail.com>
2024-10-14 15:02:53 -0700

    task done

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+==================================
+Moduli yenye darasa la BaseGeometry
+==================================
+
+Darasa hili ni tupu na halina kazi zozote.
+"""
+
+
+class BaseGeometry:
+    """Darasa tupu la BaseGeometry."""
+    pass
