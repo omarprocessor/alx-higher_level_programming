3195604b15ea758585a94e423fafe5825c27b28e
omarprocessor <wadaniinarais1@gmail.com>
2024-10-14 15:33:31 -0700

    task done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,31 +3,16 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """
 ===================================
-moduli yenye darasa la Rectangle
+module with class Rectangle
 ===================================
 """
 
 
 class Rectangle(BaseGeometry):
-    """Darasa la Rectangle ambalo linathibitisha kutoka BaseGeometry"""
+    """Rectangle class that inherits from BaseGeometry"""
 
-    def __init__(self, upana, urefu):
-        """
-        Kuanzisha Rectangle na upana na urefu.
-
-        Arguments:
-        upana -- upana wa rectangle
-        urefu -- urefu wa rectangle
-        """
-        self.integer_validator("upana", upana)
-        self.__upana = upana
-        self.integer_validator("urefu", urefu)
-        self.__urefu = urefu
-
-    def __str__(self):
-        """Kurudisha muundo wa string wa rectangle."""
-        return "[Rectangle] {}/{}".format(self.__upana, self.__urefu)
-
-    def area(self):
-        """Kurudisha eneo la rectangle."""
-        return self.__upana * self.__urefu
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
