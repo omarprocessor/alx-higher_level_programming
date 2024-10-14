97ab60ed42968a55f286747915f4d1aff72ed3f9
omarprocessor <wadaniinarais1@gmail.com>
2024-10-14 15:45:59 -0700

    task done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """
-===========================================
-Moduli yenye darasa la Rectangle
-===========================================
-
-Darasa hili linaweza kuunda rectangle
-kwa kutumia upana na urefu.
+===================================
+    Moduli yenye darasa la Rectangle
+===================================
 """
 
 
 class Rectangle(BaseGeometry):
     """
-    Darasa la Rectangle linalorithisha kutoka kwa BaseGeometry.
-
-    Inazindua rectangle na upana na urefu.
+        Darasa la Rectangle linalorithisha kutoka kwa BaseGeometry
     """
 
     def __init__(self, width, height):
-        """
-        Inizializa Rectangle na upana na urefu.
-
-        Hujumuisha:
-        - width: Upana wa rectangle, lazima iwe nambari chanya.
-        - height: Urefu wa rectangle, lazima iwe nambari chanya.
-
-        Inatupatia:
-        - Thibitisha kuwa upana na urefu ni nambari chanya
-            kwa kutumia integer_validator.
-        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def __str__(self):
-        """Kurudisha muonekano wa Rectangle kama string."""
-        return f"[Rectangle] {self.__width}/{self.__height}"
