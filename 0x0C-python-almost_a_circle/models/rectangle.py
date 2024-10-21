ef5fe4e28454ac0bdd7f03f685e05d87ac7ed5e5
omarprocessor <wadaniinarais1@gmail.com>
2024-10-21 15:53:13 -0700

    task done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
+'''Moduli ya darasa la Rectangle.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''A Rectangle class.'''
+    '''Darasa la Rectangle.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Constructor.'''
+        '''Mhandisi.'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,7 +16,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''Width of this rectangle.'''
+        '''Upana wa rectangle hii.'''
         return self.__width
 
     @width.setter
@@ -26,7 +26,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        '''Height of this rectangle.'''
+        '''Kimo cha rectangle hii.'''
         return self.__height
 
     @height.setter
@@ -36,7 +36,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        '''x of this rectangle.'''
+        '''x ya rectangle hii.'''
         return self.__x
 
     @x.setter
@@ -46,7 +46,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        '''y of this rectangle.'''
+        '''y ya rectangle hii.'''
         return self.__y
 
     @y.setter
@@ -55,32 +55,32 @@ class Rectangle(Base):
         self.__y = value
 
     def validate_integer(self, name, value, eq=True):
-        '''Method for validating the value.'''
+        '''Njia ya kuthibitisha thamani.'''
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{} lazima iwe integer".format(name))
         if eq and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
+            raise ValueError("{} lazima iwe >= 0".format(name))
         elif not eq and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
+            raise ValueError("{} lazima iwe > 0".format(name))
 
     def area(self):
-        '''Computes area of this rectangle.'''
+        '''Hesabu eneo la rectangle hii.'''
         return self.width * self.height
 
     def display(self):
-        '''Prints string representation of this rectangle.'''
+        '''Inachapisha uwakilishi wa maandiko wa rectangle hii.'''
         s = '\n' * self.y + \
             (' ' * self.x + '#' * self.width + '\n') * self.height
         print(s, end='')
 
     def __str__(self):
-        '''Returns string info about this rectangle.'''
+        '''Inarudisha habari ya maandiko kuhusu rectangle hii.'''
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
 
     def __update(self, id=None, width=None, height=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
+        '''Njia ya ndani inayosasisha sifa za mfano kupitia */**args.'''
         if id is not None:
             self.id = id
         if width is not None:
@@ -93,7 +93,7 @@ class Rectangle(Base):
             self.y = y
 
     def update(self, *args, **kwargs):
-        '''Updates instance attributes via no-keyword & keyword args.'''
+        '''Inasasisha sifa za mfano kupitia args zisizo funguo za maneno.'''
         # print(args, kwargs)
         if args:
             self.__update(*args)
@@ -101,6 +101,6 @@ class Rectangle(Base):
             self.__update(**kwargs)
 
     def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
+        '''Inarudisha uwakilishi wa kamusi wa darasa hili.'''
         return {"id": self.id, "width": self.__width, "height": self.__height,
                 "x": self.__x, "y": self.__y}
