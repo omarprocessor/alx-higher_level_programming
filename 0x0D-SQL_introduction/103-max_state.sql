6868c365e6fa648fd32ab5ecf375fd81a20b5508
omarprocessor <wadaniinarais1@gmail.com>
2024-11-19 14:05:46 -0800

    task done

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,8 +1,5 @@
 -- Select the state and the maximum temperature for each state
-SELECT state, MAX(temperature) AS max_temp
--- From the temperatures table
-FROM temperatures
--- Group by state to find the maximum temperature per state
-GROUP BY state
--- Order the results by state name in ascending order
-ORDER BY state ASC;
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
