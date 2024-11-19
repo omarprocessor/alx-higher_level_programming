cdc28ad96a625a0fadaf2cdcfff51c23236cd93c
omarprocessor <wadaniinarais1@gmail.com>
2024-11-19 14:03:30 -0800

    task done

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,13 +1,7 @@
 -- Select the city and calculate the average temperature for each city during July and August
-USE `hbtn_0c_0`
-SELECT city, AVG(temperature) AS avg_temp
--- From the table that stores temperature data
-FROM temperatures
--- Where the month is July (7) or August (8)
-WHERE MONTH(date) IN (7, 8)
--- Group by city to calculate the average for each city
-GROUP BY city
--- Order by the calculated average temperature in descending order
-ORDER BY avg_temp DESC
--- Limit the results to the top 3 cities
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
