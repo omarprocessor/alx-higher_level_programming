5eaf99853fb1d3c905a77a1ffa6a1b7b8d8d1cd0
omarprocessor <wadaniinarais1@gmail.com>
2024-11-19 13:33:10 -0800

    task done

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Script to print the full description of first_table
-SELECT TABLE_NAME, CREATE_TABLE 
-FROM information_schema.tables 
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
