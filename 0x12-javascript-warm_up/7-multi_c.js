0abaa7680cfc9c9c892fec58975b1dbbb8327d4d
Omar Mohumed <wadaniinarais1@gmail.com>
2024-12-16 16:18:01 -0800

    task done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const args = process.argv;
 if (isNaN(Number(args[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i <= Number(args[2]); i++) {
+  for (let i = 0; i < Number(args[2]); i++) {
     console.log('C is fun');
   }
 }
