6d63a688478964200f8936455c3e7571f838f20d
Omar Mohumed <wadaniinarais1@gmail.com>
2024-12-20 08:57:02 -0800

    task done

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,2 @@
 #!/usr/bin/node
-// Define the converter function
-exports.converter = function (base) {
-  return function (num) {
-    let result = '';
-    if (num === 0) return '0'; // Special case for zero
-    while (num > 0) {
-      const remainder = num % base;
-      // Handle conversion for bases greater than 10 (e.g., hexadecimal)
-      result = (remainder < 10 ? remainder : String.fromCharCode(remainder + 87)) + result;
-      num = Math.floor(num / base);
-    }
-    return result;
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
