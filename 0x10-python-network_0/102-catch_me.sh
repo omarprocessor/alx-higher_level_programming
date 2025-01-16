3c4522192e95104823106b49402d6095bbbee949
Omar Mohumed <wadaniinarais1@gmail.com>
2025-01-16 12:54:49 -0800

    task done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a PUT request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!"
-curl -s -X PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+# Sends a POST request to 0.0.0.0:5000/catch_me to get the response containing "You got me!"
+curl -sL -X POST 0.0.0.0:5000/catch_me
