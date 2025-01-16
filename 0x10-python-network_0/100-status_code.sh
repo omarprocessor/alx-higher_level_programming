8b7e1cb1001fecd94df427b708d60c1e07cf2c89
Omar Mohumed <wadaniinarais1@gmail.com>
2025-01-16 12:44:25 -0800

    task done

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Send a request to the URL passed as an argument
-# -o /dev/null: Discards the body of the response
-# -s: Silent mode, to suppress progress and error messages
-# -w "%{http_code}": Outputs only the HTTP status code of the response
+# Send a GET request to the URL and output only the HTTP status code
 curl -o /dev/null -s -w "%{http_code}" "$1"
 
