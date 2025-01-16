ab5b0a6fea9a41144691c161931d8990f8159b95
Omar Mohumed <wadaniinarais1@gmail.com>
2025-01-16 12:59:30 -0800

    task done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to 0.0.0.0:5000/catch_me to get the response containing "You got me!"
-curl -sL -X POST 0.0.0.0:5000/catch_me
+curl -s http://0.0.0.0:5000/catch_me | grep -q "You got me!" && echo "Success!" || :
