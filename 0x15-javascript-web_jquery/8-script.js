ccb87b2ae4d20b7efa6b2832ade3966111cc29da
Omar Mohumed <wadaniinarais1@gmail.com>
2025-02-24 10:13:01 -0800

    task done

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, movie) {
+    $('#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
